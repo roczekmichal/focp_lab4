84da45c5cfa37ed4d7087d3f7c32457b5443e05b
roczekmichal <mich.rocz@gmail.com>
2020-10-31 10:28:21 +0100

    Pointers: basics and pointers to arrays

--- focp_lab4/focp_lab4/focp_lab4.cpp
+++ focp_lab4/focp_lab4/focp_lab4.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
+#include <array>
+
+
 
 int main()
 {
+	/*
 	int variable = 8;
 
 	int* ptr;
@@ -14,6 +18,25 @@ int main()
 	*ptr = 20;
 
 	std::cout << "Variable changed using *ptr pointer: " << variable << std::endl;
+	*/
+
+
+	std::array <int, 4> numbers;
+
+	for (int i = 0; i < 4; i++)
+	{
+		numbers[i] = i;
+		//std::cout << numbers[i] << std::endl;
+	}
+
+	int* ptr_numbers = &numbers[0];
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << *ptr_numbers << std::endl;
+		++ptr_numbers;
+	}
+
 
 	return 0;
 }
