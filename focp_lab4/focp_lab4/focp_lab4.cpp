0ab214678270f2b0d64e75fc3d494f542a709b97
roczekmichal <mich.rocz@gmail.com>
2020-10-31 09:56:53 +0100

    Pointers basics

---
+++ focp_lab4/focp_lab4/focp_lab4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int variable = 8;
+
+	int* ptr;
+	ptr = &variable;
+
+	std::cout << "Show the value of pointer using adress of variable (&): " << *ptr << std::endl;
+
+	std::cout << "Variable not changed: " << variable << std::endl;
+
+	*ptr = 20;
+
+	std::cout << "Variable changed using *ptr pointer: " << variable << std::endl;
+
+	return 0;
+}
